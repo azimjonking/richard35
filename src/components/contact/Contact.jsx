4ecfb96fe2b6d46f21c4c640ba2f373328800a49
Suhrobbek4708 <suhrobbekolimjonov018@gmail.com>
2025-01-18 02:21:21 +0500

    footer, contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,15 @@
 import "./Contact.css";
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="contact">
+      <h1>Get updates from Richard</h1>
+      <p>New course alerts, discounts and free lessons</p>
+      <div className="inp-section">
+        <input type="text" placeholder="Enter your email" />{" "}
+        <button>Subscribe</button>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
