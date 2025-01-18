dd337b8c48e02b550e4a45d9f85d9d066db6da6b
MuhammadIsaqov <isaqovmuhammad53@gmail.com>
2025-01-18 14:25:00 +0500

    header, home

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,16 @@
 import "./Header.css";
 const Header = () => {
-  return <div>Header</div>;
+  return <div>
+    <div className="nav">
+      <div className="logo">Richard</div>
+      <ul>
+          <li><a href="/">About</a></li>
+          <li><a href="/">Features</a></li>
+          <li><a href="/">How to Use</a></li>
+          <li><a href="/">Download</a></li>
+      </ul>
+    </div>
+  </div>;
 };
 
 export default Header;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,16 @@
 import "./Home.css";
+import image from './IMAGE (31).png'
 const Home = () => {
-  return <div>Home</div>;
+  return <div className="home">
+    <div className="left">
+      <h1>Momentum</h1>
+      <p>Launch your product-and savor the momentum</p>
+      <button><a href="/">Get the App</a></button>
+    </div>
+    <div className="right">
+      <img src={image} alt="" />
+    </div>
+  </div>;
 };
 
 export default Home;
