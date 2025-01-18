c04e87667428d726bdf625076449904c262f4015
mirvaliyeva Gulhayo <mirvaliyeva Gulhayo>
2025-01-18 14:29:24 +0500

    application

--- src/components/application/Application.jsx
+++ src/components/application/Application.jsx
@@ -1,6 +1,40 @@
 import "./Application.css";
 const Application = () => {
-  return <div>Application</div>;
+  return (
+    <section className="application">
+      <div className="img"></div>
+      <div className="started">
+        <hr className="soft-wave-hr" />
+        <hr className="soft-wave-hr" />
+        <hr className="soft-wave-hr" />
+        <hr className="soft-wave-hr" />
+        <hr className="soft-wave-hr" />
+        <hr className="soft-wave-hr" />
+        <hr className="soft-wave-hr" />
+        <hr className="soft-wave-hr" />
+        <hr className="soft-wave-hr" />
+        <hr className="soft-wave-hr" />
+        <hr className="soft-wave-hr" />
+        <hr className="soft-wave-hr" />
+        <hr className="soft-wave-hr" />
+        <hr className="soft-wave-hr" />
+        <hr className="soft-wave-hr" />
+        <hr className="soft-wave-hr" />
+        <hr className="soft-wave-hr" />
+        <hr className="soft-wave-hr" />
+        <hr className="soft-wave-hr" />
+        <hr className="soft-wave-hr" />
+        <hr className="soft-wave-hr" />
+        <div className="info">
+          <h2>Ready to get started?</h2>
+          <div className="btns">
+            <button className="btn1">Download Now</button>
+            <button className="btn2">Get in Touch</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Application;
